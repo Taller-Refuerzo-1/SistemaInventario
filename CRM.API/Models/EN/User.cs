85eb77efe03c4b04215c0cc7896ebb0bccd87e56
KevinSilva <elmersilva462@gmail.com>
2024-11-13 11:06:17 -0600

    modelo de usuario

---
+++ CRM.API/Models/EN/User.cs
@@ -0,0 +1,12 @@
+﻿namespace CRM.API.Models.EN
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string Password { get; set; }
+    }
+}
